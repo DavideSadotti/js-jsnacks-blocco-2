7d6e984cfdaa1367e934bdf27fea15357c7784ff
Davide <d.sadotti@gmail.com>
2021-03-04 11:52:07 +0100

    Snack 1 con controlli

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,25 +1,32 @@
 // CICLO FOR
 var cicloUno = 0;
+var controlloUno = 0;
 
 for(var i = 0; i < 5; i++){
-  // do{
-    cicloUno += parseInt(prompt("Inserisci un numero a tua scelta!"));
-  //   console.log(cicloUno);
-  // }while(isNaN(cicloUno));
-
+  do{
+    cicloUno = parseInt(prompt("Inserisci un numero a tua scelta!"));
+    console.log(cicloUno);
+  }while(isNaN(cicloUno));
+  controlloUno += cicloUno;
 }
 
-document.getElementById("for").innerHTML = "La somma di tutti i numeri che hai inserito è di: " + cicloUno;
+document.getElementById("for").innerHTML = "La somma di tutti i numeri che hai inserito è di: " + controlloUno;
+
+
 
 // CICLO WHILE
 var cicloDue = 0;
+var controlloDue = 0;
 var j = 0;
 
 while(j < 5){
-  cicloDue += parseInt(prompt("Inserisci un numero a tua scelta!"));
-  console.log(cicloDue);
+  do{
+    cicloDue = parseInt(prompt("Inserisci un numero a tua scelta!"));
+    console.log(cicloDue);
+  }while(isNaN(cicloDue));
+  controlloDue += cicloDue;
 
   j++;
 }
 
-document.getElementById("while").innerHTML = "La somma di tutti i numeri che hai inserito è di: " + cicloDue;
+document.getElementById("while").innerHTML = "La somma di tutti i numeri che hai inserito è di: " + controlloDue;
