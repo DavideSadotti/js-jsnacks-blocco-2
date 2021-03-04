ce0d6448767d1708f29808140e40f89e9056409b
Davide <d.sadotti@gmail.com>
2021-03-04 12:55:46 +0100

    Snack 4 finito

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,1 +1,11 @@
 var interi = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+var dispari = 0;
+
+for(var i = 0; i < interi.length; i++){
+  if(i % 2 == 1){
+    dispari += interi[i];
+    document.getElementById("lista").innerHTML += "<li>Il numero da sommare è: " + interi[i] + "</li>";
+  }
+}
+
+document.getElementById("totale").innerHTML = "La somma totale fa: " + dispari;
